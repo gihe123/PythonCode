305eb2726bbbf09cd4a27fbce10c85319a87029d
Lydia <18788479@qq.com>
2017-10-21 09:53:52 +0800

    webspide

---
+++ webspider/spidimg.py
@@ -0,0 +1,7 @@
+import requests
+import bs4 from BeauifulSoap
+class Downloader(object):
+	"""docstring for Downloader"""
+	def __init__(self, arg):
+		super(Downloader, self).__init__()
+		self.arg = arg
