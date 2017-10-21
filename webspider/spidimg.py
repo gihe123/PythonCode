aeec6dd182b9f3fba0137cb9b0f06d0bb82d978e
Lydia <18788479@qq.com>
2017-10-21 10:03:24 +0800

    remove spidimg.py

--- webspider/spidimg.py
+++
@@ -1,7 +0,0 @@
-import requests
-import bs4 from BeauifulSoap
-class Downloader(object):
-	"""docstring for Downloader"""
-	def __init__(self, arg):
-		super(Downloader, self).__init__()
-		self.arg = arg
